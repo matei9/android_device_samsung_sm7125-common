4865c1b0637cadfa8a104d07395fb2540b1265b4
TestaMic <mateidiac9@gmail.com>
2025-08-31 11:36:10 +0300

    sm7125-common: Enable Flash intensity support.
    This reverts commit 160967fd1ca9c19509519adf8e17356a38c0f280.
    
    Revert "Revert "Update BoardConfigCommon.mk - Add Samsung ext SePolicy""
    
    This reverts commit a785beea4af7e119446ced26435c1ecdf6820800.
    
    Revert "Revert "Update common.mk - Add flash control""
    
    This reverts commit 429ab8dc38701017d0f224618103496d098e7e1f.
    
    Revert "Revert "Update common.mk - Add samsung ext soong namespace""
    
    This reverts commit f0e41a9b0462b9647235195fe798a9d59bc22308.
    
    Revert "Revert "Create hal_samsung_camera_flashlight_default.te - Setup SePolicy""
    
    This reverts commit 00fbc290895d22c55820b5a9792c710cd3d0ff6b.
    
    Revert "Revert "Update file.te - Setup Sepolicy""
    
    This reverts commit f26337a52a01cb4090442e756418e6d0890f3ddd.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -195,6 +195,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 # SePolicy
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+include hardware/samsung-ext/interfaces/sepolicy/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
--- common.mk
+++ common.mk
@@ -386,6 +386,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
+#Flash control
+PRODUCT_PACKAGES += \
+    vendor.samsung_ext.hardware.camera.flashlight-service \
+    FlashControl
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
@@ -439,6 +444,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/samsung-ext/interfaces \
     hardware/samsung
 
 # Inherit proprietary blobs
