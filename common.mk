834853c9da46e477d1e3d42dba425a3e33c855ca
TestaMic <mateidiac9@gmail.com>
2026-07-06 09:20:14 +0300

    sm7125-common: power-libperfmgr: Update included soong namespaces.

--- common.mk
+++ common.mk
@@ -443,7 +443,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel \
+    hardware/google/pixel/pixelstats \
+    hardware/google/pixel/power-libperfmgr \
     hardware/samsung-ext/interfaces \
     hardware/samsung
 
